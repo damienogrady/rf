3da32f4cad79101b6cb2f6dd2f50003153f907cc
Damien O'Grady <damien.ogrady@jcu.edu.au>
2015-03-16 17:44:36 +1000

    General overhaul

--- predict.R
+++ predict.R
@@ -1,8 +1,48 @@
-library(foreign)
+library(raster)
+library(rgdal)
 library(randomForest)
+library(foreign)
 
-grass<-read.dbf('bigtrees.dbf')
-use<-grass[,c(8,9,17,18,26,27,35,36,44,45,53,54,62,63,71,72,80,81)]
-class<-predict(rf,use)
-out<-data.frame(cat=grass$cat,class=as.integer(as.character(class)))
-write.dbf(out,'class.dbf')
+################################################################################
+# Random forest predictor.
+# ------------------------
+# Once you've created the random forest class object using various predictors,
+# use this function to create a raster surface predicting the class or coverage
+# results over any landscape.  The input raster "imagefile" is expected to be
+# a GeoTIFF only comprising bands representing each of the predictors.
+#
+# "bandnames" must simply be the character list of band names corresponding to
+# the predictors, IN THE ORDER THEY APPEAR IN THE GEOTIFF FILE.  These names
+# must be same as those used to create the rando forest object (obviously).
+#
+# Needless to say, "rf" is the random forst object previously created.
+# The function should create a single band GeoTIFF file with the same name as
+# "imagefile", but prefixed by "p_".
+#
+# Damien - 27 August 2014
+#
+################################################################################
+
+rfPredict <- function(rf, imagefile, bandnames, imageout=paste0("p_",imagefile)) {
+  o <- raster(imagefile,band=1) #Import the first raster band as prototype
+  nbands <- length(bandnames)
+  
+  # Now put all data into a data frame
+  data_all<-data.frame(values(raster(imagefile,band=1)))
+  
+  for(i in 2:nbands){
+    data_all<-cbind(data_all,values(raster(imagefile,band=i))) # 
+  }
+  bandnames->names(data_all)
+  
+  # Now get rid of any rows that have a null value in any of the columns
+#   for(i in 1:nbands){
+#     data_all<-data_all[!is.na(data_all[,i]),]
+#   }
+      
+  p<-predict(rf,newdata=data_all)  # Now classify the whole
+
+  values(o)<-as.numeric(p)
+  writeRaster(o,imageout,'GTiff')
+  return(p)
+}
